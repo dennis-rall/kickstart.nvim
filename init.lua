52bcebf6bed928144742be01b731cf4ce0740b91
Dennis Rall <dennis.rall@wogra.com>
2024-04-05 11:47:44 +0200

    feat: add lsp configs

--- init.lua
+++ init.lua
@@ -607,9 +607,31 @@ require('lazy').setup({
       --  See `:help lsp-config` for information about keys and how to configure
       ---@type table<string, vim.lsp.Config>
       local servers = {
+        angularls = {},
+        bashls = {},
+        cssls = {},
+        dockerls = {},
+        docker_compose_language_service = {},
+        eslint = {},
+        html = {},
+        jsonls = {},
+        quick_lint_js = {}, -- javascript
+        tsserver = {},
+        ltex = {}, -- latex
+        texlab = {}, -- latex
+        marksman = {}, -- markdown
+        -- spectral = {},      -- openapi
+        pyright = {},
+        ruff_lsp = {},
+        tailwindcss = {},
+        terraformls = {},
+        volar = {}, -- vue
+        vuels = {}, -- vue
+        hydra_lsp = {}, --yaml
+        yamlls = {},
+
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -659,7 +681,8 @@ require('lazy').setup({
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        -- You can add other tools here that you want Mason to install
+        'stylua', -- Used to format Lua code
+        'ruff',
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
