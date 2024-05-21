bc4f0cece9af0cd5390fe8474f143794d7bb1546
Dennis Rall <dennis.rall@smart-cybersecurity.de>
2024-05-21 14:09:35 +0200

    feat: enable nerd-fronts

--- init.lua
+++ init.lua
@@ -106,11 +106,8 @@ do
   -- NOTE: You can change these options as you wish!
   --  For more options, you can see `:help option-list`
 
-  -- Make line numbers default
-  vim.o.number = true
-  -- You can also add relative line numbers, to help with jumping.
-  --  Experiment for yourself to see if you like it!
-  -- vim.o.relativenumber = true
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
 
   -- Enable mouse mode, can be useful for resizing splits for example!
   vim.o.mouse = 'a'
