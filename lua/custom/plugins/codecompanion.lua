d170acc3e926b5ad11bca06f52a6798e735e2b88
Dennis Rall <dennis.rall@web.de>
2025-06-23 15:07:04 +0200

    wip: feat: add mcp hub with codecompanion integration

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -35,6 +35,20 @@ return {
       },
     },
   },
+  config = function()
+    require("codecompanion").setup({
+      extensions = {
+        mcphub = {
+          callback = "mcphub.extensions.codecompanion",
+            opts = {
+              show_result_in_chat = true,  -- Show mcp tool results in chat
+              make_vars = true,            -- Convert resources to #variables
+              make_slash_commands = true,  -- Add prompts as /slash commands
+            }
+          }
+        }
+      })
+  end,
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
