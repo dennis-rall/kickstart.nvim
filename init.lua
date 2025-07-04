e78486c306f7f704139d83fda631dcaba96fa222
Dennis Rall <dennis.rall@web.de>
2025-07-04 15:21:34 +0200

    feat: typescript only vue lsp

--- init.lua
+++ init.lua
@@ -613,7 +613,27 @@ do
               },
             },
           },
+          settings = {
+            typescript = {
+              tsserver = {
+                useSyntaxServer = false,
+              },
+              inlayHints = {
+                includeInlayParameterNameHints = 'all',
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
         },
+        -- vue_ls = {
+          -- filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+        -- },
         hydra_lsp = {}, --yaml
         yamlls = {},
 
