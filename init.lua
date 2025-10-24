bdad95ac91d11128f742d01ce125ae751a6c5f8b
Dennis Rall <dennis.rall@web.de>
2025-10-24 09:00:49 +0200

    feat: remove typescript lsp

--- init.lua
+++ init.lua
@@ -684,27 +684,7 @@ require('lazy').setup({
               },
             },
           },
-          settings = {
-            typescript = {
-              tsserver = {
-                useSyntaxServer = false,
-              },
-              inlayHints = {
-                includeInlayParameterNameHints = 'all',
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-          },
         },
-        -- vue_ls = {
-          -- filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-        -- },
         hydra_lsp = {}, --yaml
         yamlls = {},
 
