67ca313e27d74fbc4bf85157ef85a05fbad06de8
Dennis Rall <dennis.rall@web.de>
2025-10-24 09:00:49 +0200

    feat: remove typescript lsp

--- init.lua
+++ init.lua
@@ -613,27 +613,7 @@ do
               },
             },
           },
-          settings = {
-            typescript = {
-              tsserver = {
-                useSyntaxServer = false,
-              },
-              inlayHints = {
-                includeInlayParameterNameHints = 'all',
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
-            },
-          },
         },
-        -- vue_ls = {
-          -- filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-        -- },
         hydra_lsp = {}, --yaml
         yamlls = {},
 
@@ -683,6 +663,7 @@ do
           },
         },
       }
+      vim.lsp.config['ts_ls'] = {}
       ---@type MasonLspconfigSettings
       ---@diagnostic disable-next-line: missing-fields
       require('mason-lspconfig').setup {
